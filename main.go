720c023dcbc936975359a0339749ebd5a86acb14
Thiago <thiduzz14@gmail.com>
2021-01-24 12:28:14 +0100

    Migrate to Gorilla mux

--- main.go
+++ main.go
@@ -6,24 +6,32 @@ import (
 	"net/http"
 )
 
-func handleFunc(rw http.ResponseWriter, r *http.Request)  {
-	rw.Header().Set("Content-Type", "text/html")
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprint(rw, "<h1>Main page</h1>")
-		break
-	case "/contact":
-		fmt.Fprint(rw, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">Support Lenslocked.com</a>")
-		break
-	default:
-		rw.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(rw, "<h1>404</h1>")
+func home(w http.ResponseWriter, r *http.Request)   {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Main page</h1>")
+}
 
-	}
+func contact(w http.ResponseWriter, r *http.Request)   {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:support@lenslocked.com\">Support Lenslocked.com</a>")
+}
+
+func faq(w http.ResponseWriter, r *http.Request)   {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ Page</h1>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(404)
+	fmt.Fprint(w, "<h1>404</h1>")
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", handleFunc)
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/faq", faq)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000",router)
 }
