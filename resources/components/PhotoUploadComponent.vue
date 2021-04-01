b98cf8cdcb8ecd8e73e16cd6e0ae4e1cf1ebb245
Thiago <thiduzz14@gmail.com>
2021-04-01 08:16:03 +0200

    Add base photo upload component

--- resources/components/PhotoUploadComponent.vue
+++ resources/components/PhotoUploadComponent.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
-    <div class="flex w-full items-center justify-center bg-grey-lighter my-5">
+    <div v-show="file" >
+      <img ref="filePreview" src="#" alt="Selected image" />
+      <div class="my-6 pt-3 rounded bg-gray-200">
+        <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="subtitle">Subtitle</label>
+        <input v-model="subtitle" type="text" id="subtitle" name="subtitle" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
+      </div>
+    </div>
+    <div v-show="!file"  class="flex w-full items-center justify-center bg-grey-lighter my-5">
       <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue-400 hover:text-white">
         <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
           <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
         </svg>
-        <span class="mt-2 text-base leading-normal">Select a file</span>
-        <input type='file' class="hidden" />
+        <span class="mt-2 text-base leading-normal">Pick a photo</span>
+        <input ref="fileInput" @change="showPreview" type="file" class="hidden" accept="image/png, image/jpeg" />
       </label>
     </div>
-    <div class="mb-6 pt-3 rounded bg-gray-200">
-      <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="title">Title</label>
-      <input type="text" id="title" name="title" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
+    <div v-if="file" class="w-full flex justify-between">
+      <button @click="removeUpload" class="bg-red-600 px-3 hover:bg-red-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="button">Delete</button>
+      <button @click="upload" class="bg-purple-600 px-3 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="button">Save</button>
     </div>
-    <button @click="upload" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="button">Save</button>
   </div>
 </template>
 
@@ -21,18 +27,45 @@
 
 export default {
   props:{
-    galleryId: Number
+    url: String
   },
   data() {
     return {
+      file: null,
+      subtitle:''
     }
   },
-
   methods: {
     upload() {
-      debugger;
-      console.log("upload!")
+      if(this.file){
+        const formData = new FormData();
+        formData.append("image", this.file);
+        formData.append("subtitle",this.subtitle)
+        this.axios.post(this.url, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+      }
     },
+    removeUpload() {
+      this.file = null;
+      this.subtitle = '';
+      this.$refs.fileInput.value = '';
+    },
+    showPreview(){
+      this.subtitle = '';
+      if (this.$refs.fileInput.files && this.$refs.fileInput.files[0]) {
+        this.file = this.$refs.fileInput.files[0]
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.$refs.filePreview.setAttribute("src",e.target.result)
+        }
+        reader.readAsDataURL(this.file);
+      }else{
+        this.file = null;
+      }
+    }
   }
 }
 </script>
--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import PhotoUploadComponent from './components/PhotoUploadComponent.vue'
 
 const app = createApp({})
+app.use(VueAxios, axios)
 app.component('photo-upload', PhotoUploadComponent)
 app.mount('#app')
